c10e67306aeb9b10fb26ab17fe5448b600e57032
Lokon Dey <lokondey72@gmail.com>
2024-03-12 01:25:02 +0600

    login and sign up

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,15 @@ import {
 } from "react-router-dom";
 import SignUp from "./components/pages/SignUp";
 import LogIn from "./components/pages/LogIn";
+import Home from "./components/Home";
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route>
-        <Route path="/" element={<LogIn/>}></Route>
         <Route path="/signup" element={<SignUp />}></Route>
+        <Route path="/login" element={<LogIn/>}></Route>
+        <Route path="/" element={<Home/>}></Route>
       </Route>
     )
   );
